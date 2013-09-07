0b84787c2a604cd50434d0d9272b5f93986c7db7
Martin Brok Winther <martinbrokwinther@gmail.com>
2013-09-08 01:15:00 +0200

    added console.info to the stack and changed the regex so it did not break my code

--- tasks/lib/removelogging.js
+++ tasks/lib/removelogging.js
@@ -14,19 +14,23 @@ exports.init = function(grunt) {
 
     // Default methods
     if(!("methods" in opts) || !_.isArray(opts.methods)) {
-      opts.methods = "log warn error assert count clear group groupEnd groupCollapsed trace debug dir dirxml profile profileEnd time timeEnd timeStamp table exception".split(" ");
+      opts.methods = "log info warn error assert count clear group groupEnd groupCollapsed trace debug dir dirxml profile profileEnd time timeEnd timeStamp table exception".split(" ");
     }
 
-    rConsole = new RegExp(opts.namespace + ".(?:" + opts.methods.join("|") + ")\\s{0,}\\([^;]*\\)(?!\\s*[;,]?\\s*\\/\\*\\s*RemoveLogging:skip\\s*\\*\\/);?", "gi");
 
-    src = src.replace(rConsole, function() {
-      counter++;
-      return opts.replaceWith || "";
+    rConsole = new RegExp(opts.namespace + "\\.(?:" + opts.methods.join("|") + ")" + "\\(.*\\)(?:;)?((?:\\s+)?\/\\*(?:\\s+)?RemoveLogging:skip(?:\\s+)?\\*\/)?", "gi");
+  
+
+    src = src.replace(rConsole, function(value, skip) {
+    /*If skip is not an empty string or undefined the reemovelogging:skip have been matched and we will ignore it*/
+    if(typeof skip !== "undefined" && skip !== "") {return value;}
+    counter++;      
+    return opts.replaceWith || "";
     });
 
     return {
-      src: src,
-      count: counter
+        src: src,
+        count: counter
     };
   };
 };
